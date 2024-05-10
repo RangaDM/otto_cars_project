142956a2b0c9650df9ee65069bf4f24a76e68a23
Ranga Dananjaya <rangamaithreepala@gmail.com>
2024-05-10 23:02:27 +0530

    write 1st jest

--- Otto_carsale_frontend/src/pages/Login.jsx
+++ Otto_carsale_frontend/src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from "react";
-import "../styles/login.css";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import axios from "axios";
